56f0c3454bb35078b118dd020b46a671831d3b7f
stark world <nicky.kalyan438@gmail.com>
2020-10-07 17:06:19 -0400

    Day 7

---
+++ CircleOfNumbers.py
@@ -0,0 +1,13 @@
+"""
+Consider integer numbers from 0 to n - 1 written down along the circle in such a way that the distance between any
+two neighbouring numbers is equal (note that 0 and n - 1 are neighbouring, too). Given n and firstNumber,
+find the number which is written in the radially opposite position tofirstNumber. Example For n = 10 and firstNumber
+= 2, the output should be circleOfNumbers(n, firstNumber) = 7
+"""
+
+
+def circleOfNumbers(n, firstNumber):
+    return int((firstNumber + (n / 2)) % n)
+
+
+print(circleOfNumbers(20, 2))
